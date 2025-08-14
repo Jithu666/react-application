1cc7c1e1379c03222b7fb6188276bd6bed4d3681
githendragowdakr@gmail.com <githendragowdakr@gmail.com>
2025-08-14 16:17:28 +0530

    Added BlogList into Components

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,22 +1,17 @@
-const BlogList = ({ blogs, title, hideBlogs }) => {
+import { Link } from "react-router-dom";
+
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
+
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <strong>
-            Title: {blog.title}, Author: {blog.Author}
-          </strong>
-          <br />
-          <p>Description: {blog.description}</p>
-          <br />
-          <button
-            onClick={() => {
-              hideBlogs(blog.id);
-            }}
-          >
-            Hide Blog
-          </button>
+          <Link to={`/blog/${blog.id}`}>
+            <strong>{blog.title}</strong>
+            <p>Author: {blog.Author}</p>
+            <p>Description: {blog.description}</p>
+          </Link>
         </div>
       ))}
     </div>
