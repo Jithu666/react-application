6461dc5c6ba8ca93a3ba0e72b957bf84fd119de3
githendragowdakr@gmail.com <githendragowdakr@gmail.com>
2025-08-14 16:15:02 +0530

    updated jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Student from "./props/Student";
+import Create from "./components/Create";
+import BlogDetails from "./components/BlogDetails";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Uncomment Navbar and delete this line after development. */}
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/create" component={Create} />
+            {/* <Route path="/blog:id">
+              <BlogDetails />
+            </Route> */}
+            <Route path="/blog/:id" component={BlogDetails} />
+            <Route path="*">
+              <ErrorPage />
+            </Route>
+          </Switch>
+          {/* <Student /> */}
+        </div>
+      </div>
+    </Router>
   );
 }
 
